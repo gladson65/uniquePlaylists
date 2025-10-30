8cdb689d31b5f730e4af9457c91a619e2251479d
gladson65 <grauth65@gmail.com>
2025-10-30 16:26:02 +0530

    PlaylistGenerator component has been imported in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import playlistGenerator from './Components/PlaylistGenerator';
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <playlistGenerator />
     </>
   )
 }
--- src/Components/PlaylistGenerator.jsx
+++ src/Components/PlaylistGenerator.jsx
@@ -85,3 +85,5 @@ function playlistGenerator() {
         </>
     )
 }
+
+export default playlistGenerator;
